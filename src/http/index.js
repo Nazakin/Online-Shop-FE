8bd4ae0eeaf19dd6fad01ddf1568c9c775c26a78
Nazakin <134816178+Nazakin@users.noreply.github.com>
2024-07-23 02:00:04 +0200

    .

--- src/http/index.js
+++ src/http/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const $host = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://online-shop-be-qwef.onrender.com'
 })
 const $authHost = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://online-shop-be-qwef.onrender.com'
 })
 const authInterceptor = config => {
     config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
